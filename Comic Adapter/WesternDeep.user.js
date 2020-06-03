faa92a6a402280fffbffb60299d172e0765dde1a
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-06-03 20:47:48 +1000

    Update WesternDeep.user.js

--- Comic Adapter/WesternDeep.user.js
+++ Comic Adapter/WesternDeep.user.js
@@ -1,17 +1,14 @@
 // ==UserScript==
 // @name            Comic Adapter: WesternDeep
-// @version         2020.03.18
+// @version         2020.06.03
 // @description     Hotkeys
 // @include         http*://*westerndeep.net*
 // @icon            https://www.google.com/s2/favicons?domain=westerndeep.net
-// @grant           none
 // @author          Rainbow-Spike
-// @namespace       https://greasyfork.org/users/7568
-// @homepage        https://greasyfork.org/ru/users/7568-dr-yukon
+// @grant           none
 // ==/UserScript==
 
-// NUMBERS
 var prev = document.querySelector ( '.comic-nav-previous' ),
 	next = document.querySelector ( '.comic-nav-next' );
-prev.accessKey = "z";
+if ( prev != null ) prev.accessKey = "z";
 if ( next != null ) next.accessKey = "x";
