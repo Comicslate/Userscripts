bdaec7c5a546321a1215ee322aa3fc7928bb8262
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-06-03 18:08:40 +1000

    Update Freefall.user.js

--- Comic Adapter/Freefall.user.js
+++ Comic Adapter/Freefall.user.js
@@ -1,14 +1,11 @@
 // ==UserScript==
 // @name            Comic Adapter: Freefall
-// @version			2020.01.03
+// @version			2020.06.03
 // @description     Add number of stripe
 // @include         http*://freefall.purrsia.com/*
-// @author			Rainbow-Spike
-// @namespace		https://greasyfork.org/users/7568
-// @homepage		https://greasyfork.org/ru/users/7568-dr-yukon
 // @icon            https://www.google.com/s2/favicons?domain=freefall.purrsia.com
+// @author			Rainbow-Spike
 // @grant           none
-// @run-at			document-end
 // ==/UserScript==
 
 var	number = document.querySelector ( "img" ).src.match ( /f[cv]0(\d+)/ )[1], // search number of stripe in path of image
