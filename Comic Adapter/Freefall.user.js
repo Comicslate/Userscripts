0b9d7fa2f051b9e2083e4098259f14f2698cdc49
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-06-03 19:23:49 +1000

    Update Freefall.user.js

--- Comic Adapter/Freefall.user.js
+++ Comic Adapter/Freefall.user.js
@@ -1,21 +1,16 @@
 // ==UserScript==
 // @name            Comic Adapter: Freefall
 // @version			2020.06.03
-// @description     Add number of stripe
+// @description     Extract Info for Comicslate
 // @include         http*://freefall.purrsia.com/*
 // @icon            https://www.google.com/s2/favicons?domain=freefall.purrsia.com
 // @author			Rainbow-Spike
 // @grant           none
 // ==/UserScript==
 
-var	number = document.querySelector ( "img" ).src.match ( /f[cv]0(\d+)/ )[1], // search number of stripe in path of image
+var	number = document.querySelector ( "img" ).src.match ( /f[cv]0(\d+)/ )[1],
 	title = '';
 
-// search location of title of comic
-if ( window.location.href.search ( "ff" ) != -1 ) { // if non-last page
-	title = document.querySelector ( "font" );
-} else { // if main page
-	title = document.querySelector ( "table + b" );
-}
+title = document.querySelector ( window.location.href.search ( "ff" ) != -1 ? "font" : "table + b" );
 
-title.innerHTML += ' ' + number; // insert number of stripe after title of comic
+title.innerHTML += ' ' + number;
