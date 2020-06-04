bbc45e86d6e0de23f65c0355a74332429ce2b045
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-06-04 19:47:36 +1000

    Update MinorEdit.user.js

--- Comicslate/MinorEdit.user.js
+++ Comicslate/MinorEdit.user.js
@@ -15,11 +15,11 @@ var lever = 0, /*	1 - галочить только повторы
 					0 - галочить всех подряд (для генерации страниц-десятников)
 					первая правка (создание) не может быть малой!! */
 	mark = document.querySelector ( "#minoredit" ),
-	gt = document.querySelector ( "#edit__summary" ).value.search ( /(створана|създадена|oprettet|angelegt|δημιουργήθηκε|created|kreita|creado|luotu|créée|נוצר|निर्मित|dibuat|creata|作成|만듦|utworzono|criação|создано|створено|创建)/ ),
-	timer = 2000;
+	gt = document.querySelector ( "#edit__summary" ).value.search ( /(створ(ана|ено)|създадена|oprettet|angelegt|δημιουργήθηκε|cr(ea(ted|do|ta)|éée|iação)|kreita|luotu|נוצר|निर्मित|dibuat|作成|만듦|utworzono|создано|创建)/ ),
+	timer = 2;
 
 function minor ( ) {
 	if ( ( gt == -1 && !mark.checked ) || ( gt != -1 && mark.checked ) ) mark.click ( );
 }
 
-if ( mark ) lever ? setTimeout ( minor, timer ) : mark.click ( );
+if ( mark ) lever ? setTimeout ( minor, timer * 1000 ) : mark.click ( );
