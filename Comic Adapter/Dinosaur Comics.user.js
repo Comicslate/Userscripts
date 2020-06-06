8d88a295d06eea93e04ef097ce3bf499b0d130b5
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-06-07 04:19:19 +1000

    Update Dinosaur Comics.user.js

--- Comic Adapter/Dinosaur Comics.user.js
+++ Comic Adapter/Dinosaur Comics.user.js
@@ -8,8 +8,8 @@
 // @grant           none
 // ==/UserScript==
 
-var node = document.querySelector ( 'body > center' ),
-	insert = document.createElement ( 'div' ),
+var node = document.querySelector ( 'body' ),
+	insert = document.createElement ( 'span' ),
 	number = window.location.search.split ( '=' ) [ 1 ],
 	img = document.querySelector ( 'img.comic' ),
 	texter,
@@ -24,20 +24,21 @@ function selectblock ( name ) {
 	sel.addRange ( rng );
 }
 
+// HOTKEYS
+var prev = document.querySelector ( '.nohover:nth-of-type(1) a' ),
+	next = document.querySelector ( '.nohover:nth-of-type(2) a' );
+if ( prev != null ) prev.accessKey = "z";
+if ( next != null ) next.accessKey = "x";
+
 function action ( ) {
 	comm = ( img != null ) ? img.getAttribute ( 'title' ) : '';
 	texter = '== Dinosaur Comics ' + number + ' ==<br><br>{cnav}<br>{{' + number + '.png}}<br><br>';
 	texter += ( comm != ( '' | null ) ) ? comm.charAt ( 0 ).toUpperCase ( ) + comm.slice ( 1 ) + '<br>' : '';
 	texter += '{cnav}';
 	insert.innerHTML = texter;
-	( node != null ) ? node.appendChild ( insert ) : '';
+	( node != null ) ? node.insertBefore ( insert, node.firstChild ) : '';
 
+	console.log ( 'insert' );
 	selectblock ( insert );
 }
-setTimeout ( action, 300 );
-
-// HOTKEYS
-var prev = document.querySelector ( '#leftarrow' ).parentNode.parentNode,
-	next = document.querySelector ( '#rightarrow' ).parentNode.parentNode;
-if ( prev != null ) prev.accessKey = "z";
-if ( next != null ) next.accessKey = "x";
+setTimeout ( action, 100 );
