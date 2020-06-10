84fab399872f0c12328646d19eb4f376271b501e
Rainbow Spike <Dr-Yukon@users.noreply.github.com>
2020-06-11 01:59:02 +1000

    Update Dinosaur Comics.user.js

--- Comic Adapter/Dinosaur Comics.user.js
+++ Comic Adapter/Dinosaur Comics.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Comic Adapter: Dinosaur Comics
-// @version         2020.06.06
+// @version         2020.06.10
 // @description     Extract Info for Comicslate
 // @include         http*://*qwantz.com*
 // @icon            https://www.google.com/s2/favicons?domain=qwantz.com
@@ -13,7 +13,7 @@ var node = document.querySelector ( 'body' ),
 	number = window.location.search.split ( '=' ) [ 1 ],
 	img = document.querySelector ( 'img.comic' ),
 	texter,
-	comm;
+	comm, t, itle, i, tle;
 
 // SELECT
 function selectblock ( name ) {
@@ -32,13 +32,14 @@ if ( next != null ) next.accessKey = "x";
 
 function action ( ) {
 	comm = ( img != null ) ? img.getAttribute ( 'title' ) : '';
+	t = comm.charAt ( 0 ); itle = comm.slice ( 1 );
+	i = comm.charAt ( 1 ); tle = comm.slice ( 2 );
 	texter = '== Dinosaur Comics ' + number + ' ==<br><br>{cnav}<br>{{' + number + '.png}}<br><br>';
-	texter += ( comm != ( '' | null ) ) ? comm.charAt ( 0 ).toUpperCase ( ) + comm.slice ( 1 ) + '<br>' : '';
+	texter += ( comm != ( '' | null ) ) ? ( ( t.toLowerCase ( ) != t.toUpperCase ( ) ) ? ( t.toUpperCase ( ) + itle ) : ( t + i.toUpperCase ( ) + tle ) ) + '<br>' : '';
 	texter += '{cnav}';
 	insert.innerHTML = texter;
 	( node != null ) ? node.insertBefore ( insert, node.firstChild ) : '';
 
-	console.log ( 'insert' );
 	selectblock ( insert );
 }
 setTimeout ( action, 100 );
