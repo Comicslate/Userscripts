bd181ace58c0420f5dd3ae18203adfb53ffb2d39
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-07-24 03:32:39 +1000

    Update SaveEdit.user.js

--- Comicslate/SaveEdit.user.js
+++ Comicslate/SaveEdit.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name			Comicslate SaveEdit
-// @version			2020.06.28
+// @version			2020.07.23
 // @description		Закреп в макросах автовхода редактора после сохранения
-// @include			http*://*comicslate.org/ru/macro*
+// @match			http*://*comicslate.org/ru/macro*
+// @exclude			http*://browsershots.org/*
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
 // @grant			none
 // ==/UserScript==
 
-var save = document.querySelector ( "#saveedit" ),
+var save = document.querySelector ( ".saveedit input" ),
 	timer = 0.3;
 
 function action ( ) {
