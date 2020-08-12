586a6e206a33d46d0cb093e0f2c5d11ee2a89e40
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-08-12 20:10:05 +1000

    Полный пересмотр скрипта

--- Comic Adapter/FurryGuys.user.js
+++ Comic Adapter/FurryGuys.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comic Adapter: FurryGuys
-// @version			2020.06.03
+// @version			2020.08.12
 // @description		Extract Info for Comicslate
 // @include			http*://*acomics.ru/~FurryGuys/*
 // @icon			https://www.google.com/s2/favicons?domain=acomics.ru
@@ -8,48 +8,67 @@
 // @grant			none
 // ==/UserScript==
 
-var title = document.querySelector ( ".issueName" ).innerHTML, // поиск титула
-	number = document.querySelector ( ".issueNumber" ).innerHTML.split ( '/' )[ 0 ] * 1, // поиск номера
-	pic = document.querySelector ( "#mainImage" ), // поиск подписи
-	comm = document.querySelector ( ".description" ), // поиск комментария
-	entry = document.querySelector ( "nav.serial" ), // место вставки
-	texter = ''; // сборщик
+var number = document.querySelector ( ".issueNumber" ).innerHTML.split ( '/' )[ 0 ] * 1 || ( window.location.toString ( ).split ( '/' ).pop ( ) || '' ),
+	pic = document.querySelector ( "#mainImage" ) || '',
+		pictitle = pic.getAttribute ( "title" ) || '',
+		picsrc = pic.getAttribute ( "src" ).split ( '.' ) [ 1 ] || 'png',
+	title = document.querySelector ( ".issueName" ).innerHTML || ( pic.getAttribute ( "alt" ) || '' ),
+	comm = document.querySelector ( ".description" ).innerHTML || '',
+	entry = document.querySelector ( "nav.serial" ) || ( document.querySelector ( "nav" ) || '' );
 
 // ВЫДЕЛЕНИЕ
 function selectblock ( name ) {
-	var rng = document.createRange ();
+	var rng = document.createRange ( );
 	rng.selectNode ( name );
-	var sel = window.getSelection ();
-	sel.removeAllRanges ();
+	var sel = window.getSelection ( );
+	sel.removeAllRanges ( );
 	sel.addRange ( rng );
 }
 
 entry.style.cssText += "height: auto !important;";
 
-if ( title.charAt ( 2 ) == '-' ) title = 'Выпуск ' + title;
-texter += '== Furry Guys ' + number + ' ==<br>**' + title + '**<br><br>{cnav}<br>{{cotan>' + number + '.png}}<br>{{&lt;cotan}}<br>';
-
-if ( pic !== null ) pic = pic.getAttribute ( "title" );
-if ( comm !== null ) comm = comm.innerHTML;
-if ( pic !== null ) {
-	texter += '//' + pic + '//<br>';
-	if ( comm !== null ) texter += '<br>';
-};
-if ( comm !== null ) {
-	texter += comm
-	.replace ( /<p>\s*(.+)\s*<\/p>/g, "$1" ) // удаление параграфов
-	.replace ( ">&nbsp;<", "><") // без тупых пробелов
-	.replace ( /<.?span[^>]*>/g, "" ) // удаление спанов
-	.replace ( /(<br>&nbsp;)?У нас есть.+Вики-фур<\/a>(<br>)*/g, "" ) // удаление социал-ссылок
-	.replace ( /<a[^>]*href="([^"]+)"[^>]*>([^<]+)<\/a>/g, "[[$1|$2]]" ) // [[ссылки]]
-	.replace ( /<img[^>]*title="([^"]+)"[^>]*src="[^"]+emoticons[^"]+"[^>]*>/g, "$1" ) // смайлы
-	.replace ( /<em[^>]*>([^<]+)<\/em>/g, "//$1//" ) // курсив
-	.replace ( /<strong[^>]*>([^<]+)<\/strong>/g, "**$1**" ) // жирнота
-	+ "<br>"; // докувикификация
-}
-texter += '{cnav}'; // концовка вики-кода
-texter = texter.replace ( /<br>&nbsp;&nbsp;<br>/g, "<br>" ); // пустота
-
-entry.innerHTML = texter; // запись собранного вики-кода
+entry.innerHTML = '== Furry Guys '
+	+ number
+	+ ' ==<br>**'
+	+ (
+		( title.charAt ( 2 ) == '-' )
+		? 'Выпуск '
+		: ''
+	)
+	+ title
+	+ '**<br><br>{cnav}<br>{{cotan>'
+	+ number
+	+ '.'
+	+ picsrc
+	+ '}}<br>{{&lt;cotan}}<br>'
+	+ (
+		( pictitle !== '' )
+		? '//'
+			+ pictitle
+			+ '//<br>'
+			+ (
+				( comm !== '' )
+				? '<br>'
+				: ''
+			)
+		: ''
+	)
+	+ (
+		( comm !== '' )
+		? comm
+			.replace ( /<p>\s*(.+)\s*<\/p>/g, "$1" )
+			.replace ( /<.?span[^>]*>/g, "" )
+			.replace ( / style="[^"]*"/g, "" )
+			.replace ( /&nbsp;/g, " ")
+//			.trim
+			.replace ( /<br> ?У нас есть.+Вики-фур<\/a> ?(<br>)*/g, "" )
+			.replace ( /<a[^>]*href="([^"]+)"[^>]*>([^<]+)<\/a>/g, "[[$1|$2]]" )
+			.replace ( /<img[^>]*title="([^"]+)"[^>]*src="[^"]+emoticons[^"]+"[^>]*>/g, "$1" )
+			.replace ( /<em[^>]*>([^<]+)<\/em>/g, "//$1//" )
+			.replace ( /<strong[^>]*>([^<]+)<\/strong>/g, "**$1**" )
+			+ "<br>"
+		: ''
+	)
+	+ '{cnav}';
 
 selectblock ( entry );
