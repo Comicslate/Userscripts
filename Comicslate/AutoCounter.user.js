df61c603552d9640974c15ed116e68a5960bb5a5
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-08-18 08:12:17 +1000

    Update AutoCounter.user.js

--- Comicslate/AutoCounter.user.js
+++ Comicslate/AutoCounter.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name			Comicslate AutoCounter
-// @version			2020.07.23
+// @version			2020.08.18
 // @description		Автозамена номеров в Комикслейте
 // @match			http*://*comicslate.org/*do=edit*
+// @match			http*://*comicslate.org/*do=draft*
 // @exclude			http*://browsershots.org/*
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
