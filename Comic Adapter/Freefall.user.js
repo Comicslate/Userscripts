fe8352647386ebcb5bd6161c4746adc93c9f6116
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-09-25 17:59:51 +1000

    Update Freefall.user.js

--- Comic Adapter/Freefall.user.js
+++ Comic Adapter/Freefall.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Comic Adapter: Freefall
-// @version			2020.06.03
+// @version			2020.09.25
 // @description     Extract Info for Comicslate
 // @include         http*://freefall.purrsia.com/*
 // @icon            https://www.google.com/s2/favicons?domain=freefall.purrsia.com
@@ -8,9 +8,12 @@
 // @grant           none
 // ==/UserScript==
 
-var	number = document.querySelector ( "img" ).src.match ( /f[cv]0(\d+)/ )[1],
+var	number = document.querySelector ( "img" ).src.match ( /f[cv]0(\d+)/ ) [ 1 ],
 	title = '';
 
-title = document.querySelector ( window.location.href.search ( "ff" ) != -1 ? "font" : "table + b" );
-
+title = document.querySelector (
+	window.location.href.search ( "ff" ) != -1
+	? "font"
+	: "table + b"
+);
 title.innerHTML += ' ' + number;
