63d3e9cfe6e908ecaf29577616aea85f217d83c3
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-10-04 08:49:20 +1000

    Update YAFGC.user.js

--- Comic Adapter/YAFGC.user.js
+++ Comic Adapter/YAFGC.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comic Adapter: YAFGC
-// @version			2020.06.12
+// @version			2020.10.04
 // @description     Extract Info for Comicslate
 // @include			http*://*yafgc.net*
 // @icon			https://www.google.com/s2/favicons?domain=yafgc.net
@@ -8,16 +8,23 @@
 // @grant			none
 // ==/UserScript==
 
-var sidebar = document.querySelector ( "#sidebar-header" ),
-	content = document.querySelector ( ".post-content" ),
-		title = content.querySelector ( "h2" ),
-			title_a = title.querySelector ( "a" ),
-		chap = content.querySelector ( ".comic-chapter" ),
-			chap_a = chap.querySelector ( "a" ),
-		char = content.querySelector ( ".comic-characters" ),
-		loc = content.querySelector ( ".comic-locations" ),
-		ent = content.querySelector ( ".entry" ),
+var sidebar = document.querySelector ( "#sidebar-header" ) || '',
+	cont = document.querySelector ( ".post-content" ) || '',
+		title = cont.querySelector ( "h2" ) || '',
+			title_a = title.querySelector ( "a" ) || '',
+		chap = cont.querySelector ( ".comic-chapter" ) || '',
+			chap_a = chap.querySelector ( "a" ) || '',
+		char = cont.querySelector ( ".comic-characters" ) || '',
+			chart = char.textContent || '',
+		loc = cont.querySelector ( ".comic-locations" ) || '',
+			loct = loc.textContent || '',
+		ent = cont.querySelector ( ".entry" ) || '',
+			entt = ent.textContent || '',
 	texter = '';
+
+chart = chart.replace (/\s+/g, '') || '';
+loct = loct.replace (/\s+/g, '') || '';
+entt = entt.replace (/\s+/g, '') || '';
 
 // IMAGELINK
 var com_div = document.querySelector ( '#comic' ),
@@ -40,41 +47,42 @@ function selectblock ( name ) {
 }
 
 // ТИТУЛ
-if ( title_a == undefined ) title_a = title;
+if ( title_a == '' ) title_a = title;
 texter += title_a.innerHTML.replace ( /^(\d+):? (.*)$/, "== Yet Another Fantasy Gamer Comic $1 ==<br>**$2**<br><br>{cnav}<br>{{$1.jpg}}" );
-
-// ГЛАВА
 if ( chap != undefined ) texter = texter.replace ( "<br>**", "<br>**" + chap_a.innerHTML + ": " );
+if ( chart != '' || loct != '' || entt != '' ) texter += "<br><br>";
 
 // ПЕРСОНАЖИ
-if ( char != undefined ) {
-	texter += "<br><br>" + char.innerHTML
+if ( char != '' ) {
+	texter += char.innerHTML
 		.replace ( "Characters:", "Персонажи:" )
 		.replace ( /\<a href="https:\/\/www.yafgc.net\/character\/[^"]+" rel="tag"\>([^\<]+)\<\/a>/g, "[[/?do=search&id=ns%3Aru%3Agamer%3Ayet-another-fantasy-gamer-comic+$1|$1]]" );
+	if ( loct != '' || entt != '' ) texter += "\\\\<br>";
 }
 
 // МЕСТНОСТЬ
-if ( loc != undefined ) {
-	texter += ( char != undefined ) ? "\\\\<br>" : "<br><br>";
+if ( loc != '' ) {
 	texter += loc.innerHTML
 		.replace ( "Location:", "Местность:" )
 		.replace ( /\<a href="https:\/\/www.yafgc.net\/location\/[^"]+" rel="tag"\>([^\<]+)\<\/a>/g, "[[/?do=search&id=ns%3Aru%3Agamer%3Ayet-another-fantasy-gamer-comic+$1|$1]]" );
+	if ( entt != '' ) texter += "\\\\<br>";
 }
 
 // ПРОЧЕЕ
-if ( ent != undefined ) {
-	texter += ( loc == undefined ) ? "<br>" : "";
-	texter += "<br>" + ent.innerHTML
+if ( entt != '' ) {
+	texter += ent.innerHTML
 		.replace ( /\<br\>/g, "\\\\<br>" )
 		.replace ( /\<a [^/>]+ href="([^"]+)"\>([^\<]+)\<\/a>/g, "[[$1|$2]]" )
 		.replace ( /\<img src="([^"]+)"[^/>]+\>/g, "{{$1}}" )
 		.replace ( /\<em>([^\<]+)\<\/em>/g, "//$1//" )
 		.replace ( /\<strong>([^\<]+)\<\/strong>/g, "**$1**" )
+		.replace ( /\<\/p>\s+<p>/g, "\\\\<br>" )
+		.replace ( /\<\/?p>/g, "" )
+		.replace ( /\<div class="clear"><\/div>/g, "" )
 //		.replace ( /Cowboys and Crossovers”\<\/p\>\n\<p\>http:\/\/cowboysandcrossovers.thecomicseries.com\/comics\/\<\/p\>\n\<p\>Drawn/, '[[http://cowboysandcrossovers.thecomicseries.com/comics/|Cowboys and Crossovers]]"\\\\<br>Drawn' )
-	+ "{cnav}";
-} else {
-	texter += "<br>{cnav}";
 }
+
+texter += "<br>{cnav}";
 sidebar.innerHTML = texter;
 
 selectblock ( sidebar );
