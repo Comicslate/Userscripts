33d549a3b5a0a7197fdfcd335d630be350041173
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-10-21 01:20:33 +1000

    Update Index EditLinker.user.js

--- Comicslate/Index EditLinker.user.js
+++ Comicslate/Index EditLinker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate Index EditLinker
-// @version			2020.07.23
+// @version			2020.10.20
 // @description		Добавление "?do=edit" к ссылкам в "карте сайта"
 // @match			http*://*comicslate.org/*do=index*
 // @match			http*://*comicslate.org/*idx=*
@@ -10,28 +10,26 @@
 // @grant			none
 // ==/UserScript==
 
-var lang = '',
-	fromlang = '',
+var lang = '', // подменять язык
+	fromlang = '', // подменять источник перевода
 	href = 0, // для translate-раздела, замена локалей на капслоки
 	band = 0; // замена лент, 1 - d на h, 2 - наоборот
 
-document.querySelectorAll ( "#index__tree a:not(.idx_dir)" ).forEach (
-	function ( e ) {
-
-		e.href += "?do=edit" // основной функционал - ссылка на редактор
-
-		if ( lang !== '' ) e.href = e.href.replace ( /\/..\//, '/' + lang + '/' ) // если заполнен lang - подменить язык
-
-		if ( fromlang !== '' ) e.href += "&fromlang=" + fromlang // если заполнен fromlang - добавить источник разметки
-
-		if ( href ) { // для translate-раздела, замена локалей на капслоки
-			var href_split = e.href.split ( '/' ),
-				href_part = href_split[href_split.length - 1].split ( '-' );
-			if ( href_part[2] == 'local' ) e.innerHTML += ' (' + href_part[0].toUpperCase ( ) + ')'
+function action ( ) {
+	document.querySelectorAll ( "#index__tree a:not(.idx_dir)" ).forEach (
+		function ( e ) {
+			if ( e.href.search ( /\?do=edit/ ) == -1 ) e.href += "?do=edit";
+			if ( lang !== '' ) e.href = e.href.replace ( /\/..\//, '/' + lang + '/' );
+			if ( fromlang !== '' ) e.href += "&fromlang=" + fromlang;
+			if ( href ) {
+				var href_split = e.href.split ( '/' ),
+					href_part = href_split [ href_split.length - 1 ].split ( '-' );
+				if ( href_part [ 2 ] == 'local' ) e.innerHTML += ' (' + href_part [ 0 ].toUpperCase ( ) + ')'
+			};
+			if ( band == 1 ) e.href = e.href.replace ( /\/d0/, '/h0' );
+			if ( band == 2 ) e.href = e.href.replace ( /\/h0/, '/d0' );
 		}
+	)
+}
 
-		// замена лент
-		if ( band == 1 ) e.href = e.href.replace ( /\/d0/, '/h0' )
-		if ( band == 2 ) e.href = e.href.replace ( /\/h0/, '/d0' )
-	}
-)
+setInterval ( action, 100 );
