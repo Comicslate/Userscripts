dcf537f5bf6155ab34361275bcaf188d4a020b94
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2020-11-14 21:48:17 +1000

    Update Webtoons.user.js

--- Comic Adapter/Webtoons.user.js
+++ Comic Adapter/Webtoons.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Comic Adapter: Webtoons
-// @version         2020.06.03
+// @version         2020.11.14
 // @description     Extract Info for Comicslate
 // @include         http*://*webtoons.com*
 // @icon            https://www.google.com/s2/favicons?domain=webtoons.com
@@ -8,11 +8,11 @@
 // @grant           none
 // ==/UserScript==
 
-var lever = 1,
+var lever = 2,
 	timer = 300,
-	styler = 'font-size: 15px; left: 5px; position: fixed; width: 550px;',
 	nod = document.createElement ( "span" ),
 	imgs, x, img, a, num, title, comment;
+nod.style = 'font-size: 15px; left: 5px; position: fixed; top: 50px; width: 550px; ';
 
 function selectblock ( name ) {
 	var rng = document.createRange ( );
@@ -22,33 +22,39 @@ function selectblock ( name ) {
 	sel.addRange ( rng );
 }
 
-function reloader ( ) {
+function links ( lv ) {
 	nod.innerHTML = '';
 	imgs = document.querySelectorAll ( "img._images" );
 	for ( x = 0; x < imgs.length; x++ ) {
 		img = imgs [ x ].getAttribute ( 'data-url' );
-		a = document.createElement ( "a" );
-		a.href = img;
-		a.innerHTML = img;
-		a.style.display = 'block';
-		nod.appendChild ( a );
+		if ( lv ) {
+			a = document.createElement ( "a" );
+			a.href = img;
+			a.innerHTML = img;
+			a.style.display = 'block';
+			nod.appendChild ( a );
+		} else {
+			img = img.replace ( /.+\//g, "" ).replace ( /\?.+/g, "" );
+			nod.innerHTML += img + '<br>\n';
+		}
 	}
 }
 
 switch ( lever ) {
 	case 0: // LINK
-		nod.style = styler + ' text-decoration: underline; top: 50px;';
-		setInterval ( reloader, timer );
+		setInterval ( links ( 1 ), timer );
 		break
-	case 1: // SUPPLY
-		nod.style = styler + 'top: 200px;';
+	case 1: // SHORT NAMES
+		setInterval ( links ( 0 ), timer );
+		break
+	case 2: // SUPPLY
 		num = document.querySelector ( '._btnOpenEpisodeList' ).innerHTML.split ( '#' ) [ 1 ].padStart ( 4, "0" );
 		nod.innerHTML = "== ZomCom " + num + " ==<br>";
 		title = document.querySelector ( '.subj_episode' ).innerHTML.replace ( /^ ?(.*?) ?$/, '$1' );
 		if ( title != null ) nod.innerHTML +="**" + title + "**<br>";
 		nod.innerHTML += "<br>{cnav}<br>";
 		comment = document.querySelector ( '.creator_note p' );
-		if ( comment != null ) nod.innerHTML += "&lt;box unborder unbg center>" + comment.innerHTML + "\\\\ &lt;/box><br>";
+		if ( comment != null ) nod.innerHTML += "&lt;box unborder unbg center>" + comment.innerHTML.replace ( / ?$/, "" ) + "&lt;/box><br>";
 		imgs = document.querySelectorAll ( "img._images" );
 		for ( x = 0; x < imgs.length; x++ ) nod.innerHTML += "{{ " + num + "-" + ( x + 1 ) + ".jpg }}<br>";
 		if ( imgs.length == 1 ) nod.innerHTML = nod.innerHTML.replace ( "-1.", "." );
