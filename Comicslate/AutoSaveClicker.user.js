2ccac5e1dd6d24f9caf0a004dfa701b1796697a9
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-01-31 17:32:47 +1000

    Update AutoSaveClicker.user.js

--- Comicslate/AutoSaveClicker.user.js
+++ Comicslate/AutoSaveClicker.user.js
@@ -1,12 +1,17 @@
 // ==UserScript==
 // @name			Comicslate AutoSaveClicker
-// @version			2020.10.08
+// @version			2021.01.31
 // @description		Автоклик сохранения
 // @match			http*://*comicslate.org/*
+// @exclude			http*://*comicslate.org/*do=index*
+// @exclude			http*://*comicslate.org/*do=revisions*
 // @exclude			http*://browsershots.org/*
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
 // @grant			none
+// @supportURL		https://github.com/Comicslate/Userscripts/issues
+// @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSaveClicker.user.js
+// @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSaveClicker.user.js
 // ==/UserScript==
 
 var lever = 1, // генератор страниц
@@ -22,3 +27,6 @@ sv
 	)
 
 // при отсутствии реакции закрытия в фоксе - about:config -> dom.allow_scripts_to_close_windows -> true
+
+// спрятать нормальные десятники в индексах, сначала d, потом h (удалить файлы физически в тотале, генерить этим скриптом)
+// document.querySelectorAll ( '.idx .li a' ).forEach ( function ( e ) { if ( e.innerHTML.match ( 'Freefall d' ) == null ) e.style.display = 'none' } )
