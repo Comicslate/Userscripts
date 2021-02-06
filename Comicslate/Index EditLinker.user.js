e3ecb3933d6ce63801a97e9f089b3598108b2a61
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-02-06 10:35:47 +1000

    Update Index EditLinker.user.js

--- Comicslate/Index EditLinker.user.js
+++ Comicslate/Index EditLinker.user.js
@@ -1,24 +1,29 @@
 // ==UserScript==
 // @name			Comicslate Index EditLinker
-// @version			2020.10.20
-// @description		Добавление "?do=edit" к ссылкам в "карте сайта"
+// @version			2021.02.06
+// @description		Добавление do к ссылкам в "карте сайта"
 // @match			http*://*comicslate.org/*do=index*
 // @match			http*://*comicslate.org/*idx=*
 // @exclude			http*://browsershots.org/*
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
 // @grant			none
+// @supportURL		https://github.com/Comicslate/Userscripts/issues
+// @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Index EditLinker.user.js
+// @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Index EditLinker.user.js
 // ==/UserScript==
 
 var lang = '', // подменять язык
 	fromlang = '', // подменять источник перевода
 	href = 0, // для translate-раздела, замена локалей на капслоки
-	band = 0; // замена лент, 1 - d на h, 2 - наоборот
+	band = 0, // замена лент, 1 - d на h, 2 - наоборот
+	lv = 1,
+	insert = [ 'edit', 'preview', 'revisions', 'backlink' ];
 
 function action ( ) {
 	document.querySelectorAll ( "#index__tree a:not(.idx_dir)" ).forEach (
 		function ( e ) {
-			if ( e.href.search ( /\?do=edit/ ) == -1 ) e.href += "?do=edit";
+			e.href += ( e.href.search ( 'do=' + insert [ lv - 1 ] ) == -1 ) ? '?do=' + insert [ lv - 1 ] : '';
 			if ( lang !== '' ) e.href = e.href.replace ( /\/..\//, '/' + lang + '/' );
 			if ( fromlang !== '' ) e.href += "&fromlang=" + fromlang;
 			if ( href ) {
