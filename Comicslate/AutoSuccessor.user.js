0f7bea45d1cf713d4a4fe8c674ab33f790fc9c33
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-04-15 23:23:24 +1000

    Update AutoSuccessor.user.js

--- Comicslate/AutoSuccessor.user.js
+++ Comicslate/AutoSuccessor.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name			Comicslate AutoSuccessor
-// @version			2021.01.31
+// @version			2021.04.15
 // @description		Обрезка адреса после одобрения
 // @match			http*://*comicslate.org/*rev=*
 // @exclude			http*://browsershots.org/*
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
 // @grant			none
+// @supportURL		https://github.com/Comicslate/Userscripts/issues
+// @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSuccessor.user.js
+// @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSuccessor.user.js
 // ==/UserScript==
 
 if ( document.querySelector ( ".success" ) !== null ) window.location = window.location.href.split ( "?" ) [ 0 ]
