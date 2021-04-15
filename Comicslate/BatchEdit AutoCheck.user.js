749c44adee2b6456bbdee3a2d1c0de7905c31b36
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-04-15 23:22:57 +1000

    Update BatchEdit AutoCheck.user.js

--- Comicslate/BatchEdit AutoCheck.user.js
+++ Comicslate/BatchEdit AutoCheck.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name			Comicslate BatchEdit AutoCheck
-// @version			2021.01.30
+// @version			2021.04.15
 // @description		Автовыбор вариантов
 // @match			http*://*comicslate.org/*page=batchedit*
 // @exclude			http*://browsershots.org/*
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
 // @grant			none
+// @supportURL		https://github.com/Comicslate/Userscripts/issues
+// @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/BatchEdit%20AutoCheck.user.js
+// @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/BatchEdit%20AutoCheck.user.js
 // ==/UserScript==
 
 var apply = document.querySelectorAll ( ".be-apply input" ),
