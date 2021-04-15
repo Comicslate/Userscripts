fb8b04f539021df2d8351646efbe7658e23a2f94
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-04-15 23:18:22 +1000

    Update NoteSorter.user.js

--- Comicslate/NoteSorter.user.js
+++ Comicslate/NoteSorter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate NoteSorter
-// @version			2021.01.30.2
+// @version			2021.04.15
 // @description		Сортировка наклеек
 // @match			http*://*comicslate.org/*do=edit*
 // @match			http*://*comicslate.org/*do=preview*
@@ -9,6 +9,9 @@
 // @icon			https://www.google.com/s2/favicons?domain=comicslate.org
 // @author			Rainbow-Spike
 // @grant			none
+// @supportURL		https://github.com/Comicslate/Userscripts/issues
+// @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/NoteSorter.user.js
+// @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/NoteSorter.user.js
 // ==/UserScript==
 
 var wiki_text = document.querySelector ( '#wiki__text' ),
