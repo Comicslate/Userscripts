d6d9b6855746c5fd6a51546ff73be7fea40a46cc
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-05-04 02:57:26 +1000

    Create Publish DateSorter.user.js

---
+++ Comicslate/Publish DateSorter.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name			Comicslate Publish DateSorter
+// @version			2021.05.04
+// @description		Сортировка по дате
+// @match			https://comicslate.org/*/publish
+// @exclude			http*://browsershots.org/*
+// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @author			Rainbow-Spike
+// @grant			none
+// @supportURL		https://github.com/Comicslate/Userscripts/issues
+// @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Publish%20DateSorter.user.js
+// @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Publish%20DateSorter.user.js
+
+// ==/UserScript==
+
+var trs = document . querySelectorAll ( '.apr_never' ), times = [], trs1 = [];
+
+function action ( ) {
+	for ( var i = 0; i < trs . length; i++ ) {
+		times.push ( [ i, i, Date.parse ( trs [ i ] . querySelector ( '.apr_upd a' ).innerHTML . replace ( '&nbsp;', 'T' ) . replaceAll ( '/', '-' ) ) ] );
+	}
+
+	times.sort ( ( a, b ) => a [ 2 ] - b [ 2 ] );
+
+	for ( var i = 0; i < trs . length; i++ ) {
+		times [ i ] [ 0 ] = i;
+	}
+
+	for ( var i = 0; i < trs . length; i++ ) {
+		trs1 [ i ] = trs [ times [ i ] [ 1 ] ];
+	}
+
+	document . querySelector ( '.apr_table' ) . tBodies[0] . append ( ...trs1 );
+}
+
+setTimeout ( action, 3000 );
