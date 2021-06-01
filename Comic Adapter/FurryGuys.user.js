038ff9a0f7cf627c0e571b86dea2a1d171728c21
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-06-01 22:50:46 +1000

    Update FurryGuys.user.js

--- Comic Adapter/FurryGuys.user.js
+++ Comic Adapter/FurryGuys.user.js
@@ -1,87 +1,73 @@
 // ==UserScript==
 // @name			Comic Adapter: FurryGuys
-// @version			2021.01.17.1
+// @version			2021.06.01
 // @description		Extract Info for Comicslate
 // @include			http*://*acomics.ru/~FurryGuys/*
 // @icon			https://www.google.com/s2/favicons?domain=acomics.ru
 // @author			Rainbow-Spike
-// @grant			none
+// @grant			GM_setClipboard
+// @grant			GM_registerMenuCommand
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
 // @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comic%20Adapter/FurryGuys.user.js
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comic%20Adapter/FurryGuys.user.js
 // ==/UserScript==
 
-var number = window.location.toString ( ).split ( '/' ).pop ( ) || ( document.querySelector ( ".issueNumber" ).innerHTML.split ( '/' )[ 0 ] * 1 || '' ),
-	pic = document.querySelector ( "#mainImage" ) || '',
-		pictitle = pic.getAttribute ( "title" ) || '',
-		picsrc = pic.getAttribute ( "src" ).split ( '.' ) [ 1 ] || 'png',
-		title = pic.getAttribute ( "alt" ) || ( document.querySelector ( ".issueName" ).innerHTML || '' ),
-	comm = document.querySelector ( ".description" ) || '',
-	entry = document.createElement ( 'div' ),
-	place = document.querySelector ( "#content" ) || '';
+function action ( ) {
+	var number =
+		window . location . toString ( ) . split ( '/' ) . pop ( )
+		||
+		document . querySelector ( ".issueNumber" ) . innerHTML . split ( '/' ) [ 0 ] * 1
+		||
+		'',
+		pic = document . querySelector ( "#mainImage" ) || '',
+			pictitle = pic . getAttribute ( "title" ) || '',
+			picsrc = pic . getAttribute ( "src" ) . split ( '.' ) [ 1 ] || 'png',
+			title =
+				pic . getAttribute ( "alt" )
+				||
+				document . querySelector ( ".issueName" ) . innerHTML
+				||
+				'',
+		comm = document . querySelector ( ".description" ) || '',
+		texter = '';
 
-// ВЫДЕЛЕНИЕ
-function selectblock ( name ) {
-	var rng = document.createRange ( );
-	rng.selectNode ( name );
-	var sel = window.getSelection ( );
-	sel.removeAllRanges ( );
-	sel.addRange ( rng );
+	texter = '== Furry Guys '
+		+ number
+		+ ' ==\n**'
+		+ (
+			( title . charAt ( 2 ) == '-' )
+			? 'Выпуск '
+			: ''
+		)
+		+ title
+		+ '**\n\n{cnav}\n{{cotan>'
+		+ number
+		+ '.'
+		+ picsrc
+		+ '}}\n{{<cotan}}'
+		+ (
+			( pictitle !== '' )
+			? '\n//' + pictitle + '//'
+			: ''
+		)
+		+ (
+			( comm !== '' )
+			? '\n' + comm . innerHTML
+				. replace ( /<p[^>]*>\s*(<br>)*\s*(.+)\s*(<br>)*\s*<\/p>/g, "$2" )
+				. replace ( /(<.?span[^>]*>| style="[^"]*")/g, "" )
+				. replace ( /&nbsp;/g, " " )
+				. replace ( /\*(.)/g, "* $1" )
+				. replace ( /\s*(<br>)*\s*$/g, "" )
+				. replace ( /\s*(<br>)*\s*(У нас есть.+(Аск|Вики-фур|, а также страничка с эксклюзивными.+Бусти)<\/a>\s*\.?|(<strong[^>]*>)?\s*(<br>)*\s*(Спасибо, что голосуете за комикс!|В голосовалке[^<]+)\s*(<br>)*\s*(<\/strong>)?)\s*(<br>)*\s*/g, "" )
+				. replace ( /<br>/g, "\\\\\n<br />" )
+				. replace ( /\\\\\n<br \/>\\\\\n<br \/>/g, "\n<br \/>\n<br \/>" )
+				. replace ( / [-|—] /, " – " )
+				. replace ( /<a[^>]*href="([^"]+)"[^>]*>([^<]+)<\/a>/g, "[[$1|$2]]" )
+				. replace ( /<img[^>]*title="([^"]+)"[^>]*src="[^"]+emoticons[^"]+"[^>]*>/g, "$1" )
+				. replace ( /<em[^>]*>([^<]+)<\/em>/g, "//$1//" )
+				. replace ( /<strong[^>]*>([^<]+)<\/strong>/g, "**$1**" )
+			: ''
+		)
+	GM_setClipboard ( texter, "text" );
 }
-
-comm = ( comm !== '' )
-	? comm.innerHTML
-		.replace ( /<p[^>]*>\s*(<br>)*\s*(.+)\s*(<br>)*\s*<\/p>/g, "$2" )
-		.replace ( /(<.?span[^>]*>| style="[^"]*")/g, "" )
-		.replace ( /&nbsp;/g, " " )
-		.replace ( /\*(.)/g, "* $1" )
-		.replace ( /\s*(<br>)*\s*$/g, "" )
-		.replace ( /\s*(<br>)*\s*(У нас есть.+(Аск|Вики-фур|, а также страничка с эксклюзивными.+Бусти)<\/a>\s*\.?|(<strong[^>]*>)?\s*(<br>)*\s*(Спасибо, что голосуете за комикс!|В голосовалке[^<]+)\s*(<br>)*\s*(<\/strong>)?)\s*(<br>)*\s*/g, "" )
-		.replace ( /<br>/g, "\\\\\n<br />" )
-		.replace ( /\\\\\n<br \/>\\\\\n<br \/>/g, "\n<br \/>\n<br \/>" )
-		.replace ( / [-|—] /, " – " )
-		.replace ( /<a[^>]*href="([^"]+)"[^>]*>([^<]+)<\/a>/g, "[[$1|$2]]" )
-		.replace ( /<img[^>]*title="([^"]+)"[^>]*src="[^"]+emoticons[^"]+"[^>]*>/g, "$1" )
-		.replace ( /<em[^>]*>([^<]+)<\/em>/g, "//$1//" )
-		.replace ( /<strong[^>]*>([^<]+)<\/strong>/g, "**$1**" )
-	: '';
-
-entry.style = "background: #e7f3ee; border: #c7d3ce outset 3px; border-radius: 10px; margin: 2px; padding: 2px;";
-
-entry.innerHTML = '== Furry Guys '
-	+ number
-	+ ' ==<br>**'
-	+ (
-		( title.charAt ( 2 ) == '-' )
-		? 'Выпуск '
-		: ''
-	)
-	+ title
-	+ '**<br><br>{cnav}<br>{{cotan>'
-	+ number
-	+ '.'
-	+ picsrc
-	+ '}}<br>{{&lt;cotan}}<br>'
-	+ (
-		( pictitle !== '' )
-		? '//'
-			+ pictitle
-			+ '//<br>'
-			+ (
-				( comm !== '' )
-				? '<br>'
-				: ''
-			)
-		: ''
-	)
-	+ (
-		( comm !== '' )
-		? comm
-		+ '<br>'
-		: ''
-	)
-	+ '{cnav}';
-
-place.parentNode.insertBefore ( entry, place );
-
-selectblock ( entry );
+GM_registerMenuCommand ( "Start!", action );
