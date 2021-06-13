b76719078e7e048d517aa69731efb85f17ae6645
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-06-14 09:37:34 +1000

    Update CoTANizer.user.js

--- Comicslate/CoTANizer.user.js
+++ Comicslate/CoTANizer.user.js
@@ -1,35 +1,44 @@
 // ==UserScript==
 // @name			Comicslate CoTANizer
-// @version			2021.06.14
+// @version			2021.06.14.1
 // @description		Замена AIMG на CoTAN
 // @match			http*://*comicslate.org/*do=edit*
 // @match			http*://*comicslate.org/*do=preview*
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
-// @grant			none
+// @grant			GM_addStyle
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
 // @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/CoTANizer.user.js
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/CoTANizer.user.js
-
 // ==/UserScript==
 
-var wiki_text = document . querySelector ( '#wiki__text' ),
-	place = document . querySelector ( '.editButtons' );
+GM_addStyle ( '.ctn_stop{background-image:linear-gradient(to bottom,#FFF,#A9D);border-radius:3px;border:1px solid #888;padding:1px 6px;}' );
 
-function action ( ) {
+var wiki_text, place, newbtn;
+
+function ctn_repl ( ) {
 	newbtn . style . display = 'none';
-	wiki_text.value = wiki_text . value
+	wiki_text.value = wiki_text.value
 		. replace ( /(\{\{\<?)aimg(\>|\}\})/g, '$1cotan$2' )
 		. replace ( /@(.+)\n([^~]*)\n~/g, '@$1\n#\n~\n@$1\n$2\n~' );
 }
 
-if ( wiki_text . value . match ( 'aimg' ) != null ) {
-	var newbtn = document . createElement ( 'button' );
-	newbtn . id = 'replace-script';
-	newbtn . type = 'button';
-	newbtn . style = 'background-image: linear-gradient(to bottom,#FFF,#A9D); display: inline';
-	newbtn . textContent = 'CoTANize';
-	newbtn . onclick = action;
-	place . after ( newbtn );
+function ctn_start ( ) {
+	if ( !document . querySelector ( '.ctn_stop' ) ) {
+		wiki_text = document . querySelector ( '#wiki__text' );
+		place = document . querySelector ( '.editButtons' );
+		if ( wiki_text && place ) {
+			if ( wiki_text.value . match ( 'aimg' ) != null ) {
+				newbtn = document . createElement ( 'button' );
+				newbtn . type = 'button';
+				newbtn . className = 'ctn_stop';
+				newbtn . title = 'CoTANize';
+				newbtn . textContent = 'CoTANize';
+				newbtn . onclick = ctn_repl;
+				place . after ( newbtn );
+			}
+		}
+	}
 }
+
+setInterval ( ctn_start, 100 );
