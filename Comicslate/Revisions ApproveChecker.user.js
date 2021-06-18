16c0cc31b5ed03344f21656a5fd77ac2b4af9b63
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-06-19 04:51:07 +1000

    Update Revisions ApproveChecker.user.js

--- Comicslate/Revisions ApproveChecker.user.js
+++ Comicslate/Revisions ApproveChecker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate Revisions ApproveChecker
-// @version			2021.06.18
+// @version			2021.06.19
 // @description		Проверка ревизий на наличие неодобрений
 // @include			http*://comicslate.org/*do=revisions*
 // @icon			https://comicslate.org/favicon.ico
@@ -23,6 +23,7 @@ var check = document . querySelectorAll ( ".unapproved_revision" ),
 function fOpen ( i ) { window . open ( check [ i ] . querySelector ( ".wikilink1" ) . href ) };
 function fGo ( ) { window . location = parts [ 0 ] + nump + parts [ 1 ] };
 
+if ( num == 1000 || num == 2000 || num == 3000 ) window . close ( );
 if ( num == 3606 ) return;
 if ( check && check . length > 0 ) {
 	for ( var i = 0; i < check . length; i++ ) {
