c2e81a9516d65117c493dc7eaf0dcce532cbdaf2
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2021-12-10 16:11:12 +0900

    Button!

--- Comicslate/Publish DateSorter.user.js
+++ Comicslate/Publish DateSorter.user.js
@@ -1,27 +1,40 @@
 // ==UserScript==
 // @name			Comicslate Publish DateSorter
-// @version			2021.05.18
+// @version			2021.12.10
 // @description		Сортировка по дате
+// @description:en	Sort by date
 // @match			https://comicslate.org/*/publish
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
-// @grant			GM_registerMenuCommand
+// @grant			GM_addStyle
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
 // @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Publish%20DateSorter.user.js
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Publish%20DateSorter.user.js
 
 // ==/UserScript==
 
+var lever = 0; // lever 1 ascending, lever 0 descending
+
+GM_addStyle ( '#sortdates { margin-left: 4px; background-image: linear-gradient(to bottom,#7EF,#5CC); border-radius: 3px; border: 1px solid #888; }' );
+GM_addStyle ( '#sortdates:hover { background-image: linear-gradient(to bottom,#7EF,#3AB); }' );
+GM_addStyle ( '#publish { display: inline-block; }' );
+
+function h ( tag, props = {} ) {
+	return Object . assign ( document . createElement ( tag ), props );
+}
+
 function action ( ) {
-	var i, trs = document . querySelectorAll ( 'tr.apr_table' ), times = [], trs1 = [], lever = 0;
+	var i,
+		trs = document . querySelectorAll ( 'tr.apr_table' ),
+		times = [ ],
+		trs1 = [ ];
 	for ( i = 0; i < trs . length; i++ ) {
-		times.push ( [
+		times . push ( [
 			i,
-			Date.parse ( trs [ i ] . querySelector ( '.apr_upd a' ) . innerHTML . replace ( '&nbsp;', 'T' ) . replaceAll ( '/', '-' ) )
+			Date . parse ( trs [ i ] . querySelector ( '.apr_upd a' ) . innerHTML . replace ( '&nbsp;', 'T' ) . replaceAll ( '/', '-' ) )
 		] );
 	}
-	times.sort (
+	times . sort (
 		( a, b ) => lever
 		? ( a [ 1 ] - b [ 1 ] ) // lever 1 ascending
 		: ( b [ 1 ] - a [ 1 ] ) // lever 0 descending
@@ -30,6 +43,23 @@ function action ( ) {
 		trs1 [ i ] = trs [ times [ i ] [ 0 ] ];
 	}
 	document . querySelector ( 'table.apr_table' ) . tBodies [ 0 ] . append ( ...trs1 );
+	document . querySelectorAll ( '.apr_ns' ) . forEach (
+		function ( e ) {
+			e . hidden = true
+		}
+	)
 }
 
-GM_registerMenuCommand ( "Sort!", action );
+function insertButton ( ) {
+	const place = document . querySelector ( '#publish' );
+	const sortButton = h ( 'input', {
+		type: 'button',
+		value: 'Sort by date!',
+		id: 'sortdates',
+		title: 'Sort by date!',
+		onclick: ( event ) => action ( ),
+	} );
+	place . after ( sortButton );
+}
+
+insertButton ( );
