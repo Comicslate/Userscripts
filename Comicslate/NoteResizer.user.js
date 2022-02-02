d4b984e493f7edaa4716da367eb0da784657cee4
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-02-02 16:31:51 +0900

    Update NoteResizer.user.js

--- Comicslate/NoteResizer.user.js
+++ Comicslate/NoteResizer.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name			Comicslate NoteResizer
-// @version			2021.04.15
+// @version			2021.05.26
 // @description		Причёсывание наклеек под новый размер картинки
 // @match			http*://*comicslate.org/*do=edit*
 // @match			http*://*comicslate.org/*do=preview*
 // @match			http*://*comicslate.org/*do=draft*
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
 // @grant			none
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
@@ -14,31 +13,31 @@
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/NoteResizer.user.js
 // ==/UserScript==
 
-var wiki_text = document.querySelector ( '#wiki__text' ),
-	wtext = wiki_text.value.replace ( /(\d\d\d\d)\.png/, '$1_1.png' ).replace ( /\{\{([^\>\}\.]+)\.png\}\}/, '{{cotan>$1.png}}\n{{<cotan}}' ),
-	k = 900 / 522;
-wiki_text.value = wtext;
+var wiki_text = document . querySelector ( '#wiki__text' ),
+	wtext = wiki_text . value . replace ( /(\d\d\d\d)\.png/, '$1_1.png' ) . replace ( /\{\{([^\>\}\.]+)\.png\}\}/, '{{cotan>$1.png}}\n{{<cotan}}' ),
+	k = 1024 / 715;
+wiki_text . value = wtext;
 
 if (
-	( !wtext.match ( 'StopZoom' ) )
+	!wtext.match ( 'StopZoom' )
 	&&
-	( wtext.match ( 'cotan' ) != null )
+	wtext.match ( 'cotan' ) != null
 	&&
-	( wtext.match ( '}}\n{{<' ) == null )
+	wtext.match ( '}}\n{{<' ) == null
 ) {
-	var note = wtext.split ( '\n@' );
-	for ( var e = 1; e < note.length; e++ ) {
-		var strings = note [ e ].split ( '\n' ),
-			parts = strings [ 0 ].split ( ';' ),
-			coords = parts [ 0 ].split ( ',' ),
+	var note = wtext . split ( '\n@' );
+	for ( var e = 1; e < note . length; e++ ) {
+		var strings = note [ e ] . split ( '\n' ),
+			parts = strings [ 0 ] . split ( ';' ),
+			coords = parts [ 0 ] . split ( ',' ),
 			nontext = parts [ 1 ] || '';
 		for ( var i = 0; i < 4; i++ ) {
-			coords [ i ] = Math.round ( coords [ i ] * k );
+			coords [ i ] = Math . round ( coords [ i ] * k );
 		}
-		strings [ 0 ] = coords.join ( ',' );
+		strings [ 0 ] = coords . join ( ',' );
 		if ( nontext !== '' ) strings [ 0 ] += ';' + nontext;
-		note [ e ] = strings.join ( '\n' );
+		note [ e ] = strings . join ( '\n' );
 	}
-	wtext = note.join ( '\n@' );
-	wiki_text.value = wtext.replace ( '<cotan}}', '<cotan}}\n{{tag>StopZoom}}' );
+	wtext = note . join ( '\n@' );
+	wiki_text . value = wtext + '\n{{tag>StopZoom}}';
 }
