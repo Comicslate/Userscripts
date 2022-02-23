c5d784dcbaad83c9813cba3344bb91d866495b42
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-02-23 21:49:23 +0900

    Update CoTANizer.user.js

--- Comicslate/CoTANizer.user.js
+++ Comicslate/CoTANizer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate CoTANizer
-// @version			2021.05.26 (btn)
+// @version			2021.06.14.1
 // @description		Замена AIMG на CoTAN
 // @match			http*://*comicslate.org/*do=edit*
 // @match			http*://*comicslate.org/*do=preview*
@@ -14,11 +14,11 @@
 
 GM_addStyle ( '.ctn_stop{background-image:linear-gradient(to bottom,#FFF,#A9D);border-radius:3px;border:1px solid #888;padding:1px 6px;}' );
 
-var wiki_text, wtext, place, newbtn;
+var wiki_text, place, newbtn;
 
 function ctn_repl ( ) {
 	newbtn . style . display = 'none';
-	wiki_text.value = wtext
+	wiki_text . value = wiki_text . value
 		. replace ( /(\{\{\<?)aimg(\>|\}\})/g, '$1cotan$2' )
 		. replace ( /@(.+)\n([^~]*)\n~/g, '@$1\n#\n~\n@$1\n$2\n~' );
 }
@@ -28,8 +28,7 @@ function ctn_start ( ) {
 		wiki_text = document . querySelector ( '#wiki__text' );
 		place = document . querySelector ( '.editButtons' );
 		if ( wiki_text && place ) {
-			wtext = wiki_text . value;
-			if ( wtext . match ( 'aimg' ) != null ) {
+			if ( wiki_text . value . match ( 'aimg' ) != null ) {
 				newbtn = document . createElement ( 'button' );
 				newbtn . type = 'button';
 				newbtn . className = 'ctn_stop';
