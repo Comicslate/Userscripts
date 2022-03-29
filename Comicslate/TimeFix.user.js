b0da826b07d197d1a90bacd5f6ae58f04eaee737
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-03-29 22:29:28 +0900

    Update TimeFix.user.js

--- Comicslate/TimeFix.user.js
+++ Comicslate/TimeFix.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate TimeFix
-// @version			2022.03.26
+// @version			2022.03.29
 // @description		Исправление часового пояса на Комикслейте
 // @match			http*://*comicslate.org/*
 // @icon			https://comicslate.org/favicon.ico
@@ -14,57 +14,25 @@
 var workPlace = document . querySelectorAll ( ".pageinfo, .date, .diffnav option, th a, .approval_date, .approval_previous, .apr_upd, .apr_prev, .sum, #draft__status" );
 
 function timeFix ( ) {
-	var timeShift = localStorage . getItem ( 'timezone' ) * 1;
+	var regexp = /(\d\d\d\d)\/(\d\d)\/(\d\d) (\d\d):(\d\d)/;
 	for ( var i in workPlace ) {
 		if ( workPlace [ i ] . innerHTML != undefined ) {
-			var timeDigits = workPlace [ i ] . innerHTML . match ( /(\d\d\d\d)\/(\d\d)\/(\d\d) (\d\d):/ );
+			var timeDigits = workPlace [ i ] . innerHTML . match ( regexp );
 			if ( timeDigits != null ) {
-				var newHour = +timeDigits [ 4 ] + timeShift,
-					fixHour = newHour % 24 + ( newHour < 0 ? 24 : 0 ),
-					newDay = +timeDigits [ 3 ] + Math . floor ( newHour / 24 ),
-					oldMonth = +timeDigits [ 2 ],
-					oldYear = +timeDigits [ 1 ],
-					monthLength = '';
-				switch ( oldMonth ) {
-					case 1:
-					case 3:
-					case 5:
-					case 7:
-					case 8:
-					case 10:
-					case 12:
-						monthLength = 31;
-						break;
-					case 2:
-						if ( oldYear % 4 == 0 ) {
-							monthLength = 29;
-						} else {
-							monthLength = 28;
-						};
-						break;
-					case 4:
-					case 6:
-					case 9:
-					case 11:
-						monthLength = 30;
-						break;
-				};
-				var fixDay = newDay % monthLength;
-				if ( fixDay == 0 ) fixDay = monthLength;
-				var newMonth = oldMonth + Math . ceil ( newDay / monthLength - 1 ),
-					fixMonth = newMonth % 12;
-				if ( fixMonth == 0 ) fixMonth = 12;
-				var newYear = oldYear + Math . ceil ( newMonth / 12 - 1 );
-				workPlace [ i ] . innerHTML = workPlace [ i ] . innerHTML . replace (
-					/(\d\d\d\d)\/(\d\d)\/(\d\d) (\d\d):/,
-					newYear
-					+ '/'
-					+ ( ( fixMonth < 10 ) ? ( '0' + fixMonth ) : fixMonth )
-					+ '/'
-					+ ( ( fixDay < 10 ) ? ( '0' + fixDay ) : fixDay )
-					+ '&nbsp;'
-					+ ( ( fixHour < 10 ) ? ( '0' + fixHour ) : fixHour )
-					+ ':' );
+				var newTime = new Date ( Date . parse ( timeDigits [ 1 ] + '-' + timeDigits [ 2 ] + '-' + timeDigits [ 3 ] + 'T' + timeDigits [ 4 ] + ':' + timeDigits [ 5 ] + 'Z' ) ),
+					nParts = {
+					   year:    newTime . getFullYear ( ),
+					   month: ( newTime . getMonth ( ) + 1 ),
+					   day:     newTime . getDate ( ),
+					   hour:    newTime . getHours ( ),
+					   minute:  newTime . getMinutes ( ),
+					},
+					newParts = { };
+				for ( var n in nParts ) {
+				   newParts [ n ] = ( parseInt ( nParts [ n ], 10 ) < 10 ) ? ( '0' + nParts [ n ] ) : ( nParts [ n ] );
+				}
+				newTime = newParts . year + '/' + newParts . month + '/' + newParts . day + '&nbsp;' + newParts . hour + ':' + newParts . minute;
+				workPlace [ i ] . innerHTML = workPlace [ i ] . innerHTML . replace ( regexp, newTime )
 				workPlace [ i ] . style . textShadow = '#000 0 .5px .5px';
 			}
 		}
