7527a3755275b1144f432a2008ad53309054b680
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-01 21:35:30 +0900

    Update Index EditLinker.user.js
    полная переделка на кнопочное управление (справа внизу экрана)

--- Comicslate/Index EditLinker.user.js
+++ Comicslate/Index EditLinker.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name			Comicslate Index EditLinker
-// @version			2021.04.15
+// @version			2022.05.01
 // @description		Добавление do к ссылкам в "карте сайта"
 // @match			http*://*comicslate.org/*do=index*
 // @match			http*://*comicslate.org/*idx=*
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
 // @grant			none
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
@@ -13,28 +12,106 @@
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/Index%20EditLinker.user.js
 // ==/UserScript==
 
-var lang = '', // подменять язык
-	fromlang = '', // подменять источник перевода
-	href = 0, // для translate-раздела, замена локалей на капслоки
-	band = 0, // замена лент, 1 - d на h, 2 - наоборот
-	lv = 1,
-	insert = [ 'edit', 'preview', 'revisions', 'backlink' ];
-
-function action ( ) {
-	document.querySelectorAll ( "#index__tree a:not(.idx_dir)" ).forEach (
-		function ( e ) {
-			e.href += ( e.href.search ( 'do=' + insert [ lv - 1 ] ) == -1 ) ? '?do=' + insert [ lv - 1 ] : '';
-			if ( lang !== '' ) e.href = e.href.replace ( /\/..\//, '/' + lang + '/' );
-			if ( fromlang !== '' ) e.href += "&fromlang=" + fromlang;
+/*var href = 0, // для translate-раздела, замена локалей на капслоки
 			if ( href ) {
-				var href_split = e.href.split ( '/' ),
-					href_part = href_split [ href_split.length - 1 ].split ( '-' );
-				if ( href_part [ 2 ] == 'local' ) e.innerHTML += ' (' + href_part [ 0 ].toUpperCase ( ) + ')'
-			};
-			if ( band == 1 ) e.href = e.href.replace ( /\/d0/, '/h0' );
-			if ( band == 2 ) e.href = e.href.replace ( /\/h0/, '/d0' );
+				var href_split = e . href . split ( '/' ),
+					href_part = href_split [ href_split . length - 1 ] . split ( '-' );
+				if ( href_part [ 2 ] == 'local' ) e . innerHTML += ' (' + href_part [ 0 ] . toUpperCase ( ) + ')'
+			};*/
+
+function insertAttr ( action ) {
+	document . querySelectorAll ( "#index__tree a:not(.idx_dir)" ) . forEach (
+		function ( e ) {
+			e . href = e . href . split ( '?' ) [ 0 ] + '?do=' + action;
 		}
 	)
 }
 
-setInterval ( action, 100 );
+function insertLang ( ill1 ) {
+	document . querySelectorAll ( "#index__tree a:not(.idx_dir)" ) . forEach (
+		function ( e ) {
+			e . href = e . href . replace ( /\/s?..\//, '/' + ill1 + '/' );
+		}
+	)
+}
+
+function insertFromLang ( ilf1 ) {
+	document . querySelectorAll ( "#index__tree a:not(.idx_dir)" ) . forEach (
+		function ( e ) {
+			e . href = e . href . split ( '?' ) [ 0 ] + '?do=edit&fromlang=' + ilf1;
+		}
+	)
+}
+
+function h ( tag, props = { }, children = [ ] ) {
+	const element = Object . assign ( document . createElement ( tag ), props );
+	element . append ( ...children );
+	return element;
+}
+
+function insertButtons ( ) {
+	var indexLinkDiv = h ( 'div', {
+		className: 'indexlink',
+		style: 'position: fixed; bottom: 5px; right: 5px;'
+	}, [
+		h ( 'div', {
+			id: 'indexlink-top'
+		}, [
+			h ( 'input', {
+				type: 'button',
+				value: 'lang = ',
+				style: 'margin: 5px;',
+				onclick: ( event ) => insertLang ( indexLinkLang . value )
+			} ),
+			h ( 'input', {
+				type: 'text',
+				value: lang,
+				id: 'indexLinkLang',
+				style: 'margin: 5px; width: 20px; text-align: center;',
+			} ),
+			h ( 'input', {
+				type: 'button',
+				value: 'fromlang = ',
+				style: 'margin: 5px;',
+				onclick: ( event ) => insertFromLang ( indexLinkFLng . value )
+			} ),
+			h ( 'input', {
+				type: 'text',
+				value: 'en',
+				id: 'indexLinkFLng',
+				style: 'margin: 5px; width: 20px; text-align: center;',
+			} )
+		] ),
+		h ( 'div', {
+			className: 'indexlink-bottom'
+		}, [
+			h ( 'input', {
+				type: 'button',
+				value: 'edit',
+				style: 'margin: 5px;',
+				onclick: ( event ) => insertAttr ( 'edit' )
+			} ),
+			h ( 'input', {
+				type: 'button',
+				value: 'preview',
+				style: 'margin: 5px;',
+				onclick: ( event ) => insertAttr ( 'preview' )
+			} ),
+			h ( 'input', {
+				type: 'button',
+				value: 'revisions',
+				style: 'margin: 5px;',
+				onclick: ( event ) => insertAttr ( 'revisions' )
+			} ),
+			h ( 'input', {
+				type: 'button',
+				value: 'backlink',
+				style: 'margin: 5px;',
+				onclick: ( event ) => insertAttr ( 'backlink' )
+			} )
+		] )
+	] );
+	document . body . appendChild ( indexLinkDiv );
+}
+
+insertButtons ( );
