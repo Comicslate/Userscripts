acecdeeea1cf3bbe12aa38b24eb9ddcf437cecb0
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-13 00:32:21 +1000

    Update AutoSaveClicker.user.js
    лимит в 2 секунды

--- Comicslate/AutoSaveClicker.user.js
+++ Comicslate/AutoSaveClicker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate AutoSaveClicker
-// @version			2021.05.28
+// @version			2021.06.15
 // @description		Автоклик сохранения
 // @include			/comicslate.org.+(sci-fi|tlk|wolves|mlp|furry|gamer|other|interrobang)/
 // @exclude			/^https?://comicslate\.org\/.+do=[^e]+/
@@ -15,15 +15,15 @@
 
 var sum = document . querySelector ( "#edit__summary" ),
 
-	autosave = 1,
+	autosave = 0,
 	sv = document . querySelector ( "#edbtn__save" ),
 
-	autoclose = 1, // для NaviClicker - 0, для IndexEditLinker - 1
-	timer = 10;
+	autoclose = 1, // для NaviClicker - 0, для Index EditLinker - 1
+	timer = 2; // ≥2s!
 
 if ( sum && sum . value != null ) {
 	if ( sum . value != '' ) sum . value += ' / ';
-	sum . value += 'AutoSaveClicker 2021.05.26';
+	sum . value += 'ASC 2021.05.30';
 }
 
 ( sv )
@@ -33,7 +33,7 @@ if ( sum && sum . value != null ) {
 			: ''
 	)
 	: (
-		( autoclose && timer > 2 )
+		( autoclose && timer >= 2 )
 			? setTimeout ( function ( ) { window . close ( ) }, timer * 1000 ) // в Фаерфоксе требуется about:config -> dom.allow_scripts_to_close_windows -> true
 			: ''
 	)
