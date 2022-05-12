03b1c4362fbaa3616c7c553abf97c06ca39a9ff7
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-13 00:19:30 +1000

    Update AutoApprove.user.js
    прореживание и Аркейн

--- Comicslate/AutoApprove.user.js
+++ Comicslate/AutoApprove.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name			Comicslate AutoApprove
-// @version			2021.04.20
+// @version			2021.12.30
 // @description		Автоодобрение правок
 // @match			http*://*comicslate.org/*
 // @exclude			http*://*comicslate.org/*do=*
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
 // @grant			none
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
@@ -13,16 +12,21 @@
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoApprove.user.js
 // ==/UserScript==
 
-var approve_link = document.querySelector ( ".approval_action a" ),
+var approve_link = document . querySelector ( ".approval_action a" ),
 	self = "Robot Spike",
-	lever = 1; // 0 - одобрять всё, 1 - только себя
+	fr = "Arkane",
+	lever = 0; // 0 - одобрять всё, 1 - только себя и Аркейна
 
 if ( approve_link != null ) {
 	( lever )
 	? (
-		( document.querySelectorAll ( ".pageinfo bdi" ) [ 1 ].innerHTML == self )
-		? approve_link.click ( )
+		(
+			document . querySelectorAll ( ".pageinfo bdi" ) [ 1 ] . innerHTML == self
+			||
+			document . querySelectorAll ( ".lang-fr .pageinfo bdi" ) [ 1 ] . innerHTML == fr
+		)
+		? approve_link . click ( )
 		: ''
 	)
-	: approve_link.click ( )
+	: approve_link . click ( )
 }
