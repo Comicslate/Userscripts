9da0c7043370f7e553b0555558b01cdca40a7de1
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-13 00:50:02 +1000

    Update MinorEdit.user.js
    прореживание

--- Comicslate/MinorEdit.user.js
+++ Comicslate/MinorEdit.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name			Comicslate MinorEdit
-// @version			2021.04.15
+// @version			2021.04.26
 // @description		Автовыбор "малой правки"
 // @match			http*://*comicslate.org/*do=edit*
 // @match			http*://*comicslate.org/*do=draft*
 // @exclude			http*://*comicslate.org/*news*
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
 // @grant			none
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
@@ -17,12 +16,12 @@
 var lever = 1, /*	1 - галочить только исправления, создания не галочить
 					0 - галочить всех подряд (для генерации страниц-десятников)
 					первая правка (создание) не может быть малой!! */
-	mark = document.querySelector ( "#minoredit" ),
-	gt = document.querySelector ( "#edit__summary" ).value.search ( /(створ(ана|ено)|създадена|oprettet|angelegt|δημιουργήθηκε|cr(ea(ted|do|ta)|éée|iação)|kreita|luotu|נוצר|निर्मित|dibuat|作成|만듦|utworzono|создано|创建)/ ),
+	mark = document . querySelector ( "#minoredit" ),
+	gt = document . querySelector ( "#edit__summary" ) . value . search ( /(створ(ана|ено)|създадена|oprettet|angelegt|δημιουργήθηκε|cr(ea(ted|do|ta)|éée|iação)|kreita|luotu|נוצר|निर्मित|dibuat|作成|만듦|utworzono|создано|创建)/ ),
 	timer = 2;
 
 function minor ( ) {
-	if ( ( gt == -1 && !mark.checked ) || ( gt != -1 && mark.checked ) ) mark.click ( );
+	if ( ( gt == -1 && !mark.checked ) || ( gt != -1 && mark.checked ) ) mark . click ( );
 }
 
-if ( mark ) lever ? setTimeout ( minor, timer * 1000 ) : mark.click ( );
+if ( mark ) lever ? setTimeout ( minor, timer * 1000 ) : mark . click ( );
