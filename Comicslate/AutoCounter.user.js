4d6f683bf8270169cd761f75ba6b16e89e8c84c4
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-13 00:25:28 +1000

    Update AutoCounter.user.js
    переписано и прорежено

--- Comicslate/AutoCounter.user.js
+++ Comicslate/AutoCounter.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name			Comicslate AutoCounter
-// @version			2021.04.15.2
+// @version			2021.05.26
 // @description		Автозамена номеров в Комикслейте
 // @match			http*://*comicslate.org/*do=edit*
+// @match			http*://*comicslate.org/*do=preview*
 // @match			http*://*comicslate.org/*do=draft*
 // @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
 // @grant			none
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
@@ -13,23 +14,22 @@
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoCounter.user.js
 // ==/UserScript==
 
-var wiki__text = document.querySelector ( "#wiki__text" ),
-	hn_num = window.location.pathname.split ( /\// ).pop()%7;
+var wiki__text = document . querySelector ( "#wiki__text" ),
+	lhref = window . location . href;
 
 if ( wiki__text !== null ) {
-	var text = wiki__text.innerHTML;
+	var text = wiki__text . innerHTML;
 
-	if ( window.location.href.match ( 'western-deep' ) !== null ) {
-		var num = text.match ( /Western Deep (\d+)/ )[1] * 1 - 382;
-		if ( num !== null ) text = text.replace ( 'Страница *', 'Страница ' + num + '*' );
+	if ( lhref . match ( 'western-deep' ) !== null ) {
+		var num = text . match ( /Western Deep (\d+)/ ) [ 1 ] * 1 - 382;
+		if ( num !== null ) text = text . replace ( 'Страница *', 'Страница ' + num + '*' );
 	}
-	if ( window.location.href.match ( 'furry-guys' ) !== null ) {
-		if ( window.location.href.match ( '\/en\/' ) !== null ) text = text.replace ( 'Выпуск', 'Issue' )
-	}
-	if ( window.location.href.match ( 'heavenly-nostrils' ) !== null
+	if ( lhref . match ( /\/en\/.+\/furry-guys/ ) !== null ) text = text . replace ( 'Выпуск', 'Issue' );
+	if (
+		lhref . match ( 'heavenly-nostrils' ) !== null
 		&&
-		hn_num == 1
-	   ) text = text.replace ( 'gif', 'jpg' );
+		window . location . pathname . split ( /\// ) . pop ( ) % 7 == 1
+	   ) text = text . replace ( 'gif', 'jpg' );
 
-	wiki__text.innerHTML = text;
+	wiki__text . innerHTML = text;
 }
