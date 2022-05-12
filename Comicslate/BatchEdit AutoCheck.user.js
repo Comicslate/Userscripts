2f7e946c99d1de5866b97e329af9ee418eca0933
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-13 00:43:20 +1000

    Update BatchEdit AutoCheck.user.js
    прореживание, заметка о лентах

--- Comicslate/BatchEdit AutoCheck.user.js
+++ Comicslate/BatchEdit AutoCheck.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name			Comicslate BatchEdit AutoCheck
-// @version			2021.04.15
+// @version			2021.09.11
 // @description		Автовыбор вариантов
 // @match			http*://*comicslate.org/*page=batchedit*
-// @exclude			http*://browsershots.org/*
-// @icon			https://www.google.com/s2/favicons?domain=comicslate.org
+// @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
 // @grant			none
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
@@ -12,18 +11,22 @@
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/BatchEdit%20AutoCheck.user.js
 // ==/UserScript==
 
-var apply = document.querySelectorAll ( ".be-apply input" ),
-	minor = document.querySelector ( "#be-minor" );
+var apply = document . querySelectorAll ( ".be-apply input" ),
+	minor = document . querySelector ( "#be-minor" );
 
 function action ( cl ) {
-	if ( cl.length !== undefined ) {
-		for ( var i = 0; i < cl.length; i++ ) {
-			if ( cl [ i ].checked == false ) cl [ i ].click ( );
+	if ( cl . length !== undefined ) {
+		for ( var i = 0; i < cl . length; i++ ) {
+			if ( cl [ i ] . checked == false ) cl [ i ] . click ( );
 		}
 	} else {
-		if ( cl.checked == false ) cl.click ( );
+		if ( cl . checked == false ) cl . click ( );
 	}
 }
 
 setInterval ( action ( minor ), 300 );
 setInterval ( action ( apply ), 300 );
+
+/*
+document . querySelectorAll ( '.be-stats .be-apply label[for*="0001"]' ) . forEach ( function ( e ) { e . previousSibling . previousSibling . click ( ) } )
+*/
