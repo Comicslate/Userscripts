aaec9f4eddae8393a25468a00d5fcb266b881a6f
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2022-05-24 13:44:09 +1000

    Переделка на скрипт с экранной кнопкой активации

--- Comic Adapter/Little Bobby.user.js
+++ Comic Adapter/Little Bobby.user.js
@@ -1,28 +1,52 @@
 // ==UserScript==
 // @name			Comic Adapter: Little Bobby
-// @version			2020.06.03
+// @version			2022.05.22
 // @description		Extract Info for Comicslate
-// @include			http*://*littlebobbycomic.com*
+// @match			http*://www.littlebobbycomic.com/*
 // @icon			https://www.google.com/s2/favicons?domain=littlebobbycomic.com
 // @author			Rainbow-Spike
-// @grant			none
+// @grant           GM_setClipboard
+// @supportURL      https://github.com/Comicslate/Userscripts/issues
+// @updateURL       https://github.com/Comicslate/Userscripts/raw/master/Comic%20Adapter/Little%20Bobby.user.js
+// @downloadURL     https://github.com/Comicslate/Userscripts/raw/master/Comic%20Adapter/Little%20Bobby.user.js
 // ==/UserScript==
 
-var strip = document.querySelector ( '.slides img, .wp-post-image'),
-	img = strip.getAttribute ( 'src' ),
-	dlink = document.createElement ( 'a' );
+const prev = document . querySelector ( 'a[rel="prev"]' ),
+	next = document . querySelector ( 'a[rel="next"]' ),
+	img = document . querySelector ( ".slides img, .wp-post-image" ) . getAttribute ( 'src' );
+if ( prev ) prev . accessKey = "z";
+if ( next ) next . accessKey = "x";
 
-// SELECT
-function selectblock ( name ) {
-	var rng = document.createRange ( );
-	rng.selectNode ( name );
-	var sel = window.getSelection ( );
-	sel.removeAllRanges ( );
-	sel.addRange ( rng );
+function h ( tag, props = { } ) {
+	return Object . assign ( document . createElement ( tag ), props );
 }
 
-dlink.setAttribute ( 'href', img );
-dlink.innerHTML = img;
-strip.parentNode.parentNode.appendChild ( dlink );
+/* Button click link copy */
+function action ( ) {
+	GM_setClipboard ( img );
+}
 
-selectblock ( dlink );
+function insertButton ( ) {
+	const Button = h ( 'input', {
+		type: 'button',
+		value: '[[ ]]',
+		title: '[[ ]]',
+		style: 'position: fixed; bottom: 20px; left: 50px;',
+		onclick: ( event ) => action ( ),
+	} );
+	document . body . appendChild ( Button );
+}
+
+insertButton ( );
+
+/* Automated link copy */
+/* function insertLink ( ) {
+	const Link = h ( 'span', {
+		innerHTML: 'Link',
+		style: 'position: fixed; bottom: 20px; left: 50px;',
+		onclick: GM_setClipboard ( img ),
+	} );
+	document . body . appendChild ( Link );
+}
+
+insertLink ( ); */
