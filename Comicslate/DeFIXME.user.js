bafa62ee3e3d01e3419864b2e23e8849113d79d9
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2023-11-22 10:27:45 +0900

    Update DeFIXME.user.js
    -запрос по адресу
    +удаление вторых FIXME (надо бы перенести в ядро)

--- Comicslate/DeFIXME.user.js
+++ Comicslate/DeFIXME.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate DeFIXME
-// @version			2022.07.04
+// @version			2023.11.22
 // @description		Удаление FIXME
 // @match			http*://*comicslate.org/*do=edit*
 // @match			http*://*comicslate.org/*do=preview*
@@ -13,11 +13,12 @@
 // ==/UserScript==
 
 var wiki_text = document . querySelector ( '#wiki__text' ),
-	lhref = window . location . href,
-	text = /FIXME \*\*[^\n]+[\)）]\/\/\n\n/;
+	path = document . querySelector ( '.dokuwiki.lang-en.freefall' ) || document . querySelector ( '.dokuwiki.band' ),
+	text = /FIXME \*\*[^\n]+[\)）]\/\/\n\n/,
+	text2 = RegExp ( text . source, "g");
 
-if (
-	/FIXME/ . test ( wiki_text . value )
-	&&
-	/(\/[dh]\d+|\/en\/sci\-fi\/freefall)/ . test ( lhref ) /* в лентах и англ. фрифоле */
-) wiki_text . value = wiki_text . value . replace ( text, '' );
+if ( wiki_text && /FIXME/ . test ( wiki_text . value ) ) {
+	if ( path ) wiki_text . value = wiki_text . value . replace ( text, '' ); /* в лентах и англ. фрифоле */
+	var trys = wiki_text . value . match ( text2 );
+	if ( trys . length > 1 ) wiki_text . value = wiki_text . value . replace ( trys [ 1 ], '' ); /* повторный FIXME */
+}
