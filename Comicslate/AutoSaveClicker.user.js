7207f7f3f53850f1f0f7b8c6511b6bb0a7d55854
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2025-06-23 22:53:25 +0900

    Update AutoSaveClicker.user.js
    обновление

--- Comicslate/AutoSaveClicker.user.js
+++ Comicslate/AutoSaveClicker.user.js
@@ -1,33 +1,43 @@
 // ==UserScript==
 // @name			Comicslate AutoSaveClicker
-// @version			2025.06.22.2
+// @version			2025.06.23
 // @description		Автоклик сохранения + удалялка
 // @match			http*://*comicslate.org/*
 // @exclude			/^https?://comicslate\.org\/.+publish
 // @icon			https://comicslate.org/favicon.ico
 // @author			Rainbow-Spike
-// @grant			none
+// @grant			window.close
 // @supportURL		https://github.com/Comicslate/Userscripts/issues
 // @updateURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSaveClicker.user.js
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSaveClicker.user.js
 // ==/UserScript==
 
-var wiki__text = document . querySelector ( ".comic.editpage #wiki__text" ),
-	stop = document . querySelector ( "#dw__recent" ),
-	save = document . querySelector ( "#edbtn__save" ),
-	sum = document . querySelector ( "#edit__summary" ),
+const cf = document . querySelector ( "#cf-wrapper" ),
+	editor = document . querySelector ( ".comic.editpage" ),
 	autoeraser = 0, // для удаления текста - 1 !! ВНИМАНИЕ
 	autosave = 1, // для сохранения - 1, для простого посещения страницы - 0
 	autoclose = 1, // для NaviClicker - 0, для IndexEditLinker - 1
-	timer = 4;
+	save_timer = 4,
+	close_timer = 1;
 
-if ( wiki__text != null ) {
-	if ( sum && sum . value != null ) {
-		if ( sum . value != '' ) sum . value += ' / ';
-		sum . value += 'ASC 2025.06.22';
+// защита от ошибки 1015
+if ( cf != null ) setTimeout ( function ( ) { location . reload ( ) }, 1000 );
+
+if ( editor != null ) {
+	let wiki__text = editor . querySelector ( "#wiki__text" );
+	if ( wiki__text != null ) {
+		const sum = editor . querySelector ( "#edit__summary" );
+		if ( sum && sum . value != null ) {
+			if ( sum . value != '' ) sum . value += ' / ';
+			sum . value += 'ASCBot 2025.06.22';
+		}
+		if ( autoeraser ) wiki__text . value = '';
+		const save = editor . querySelector ( "#edbtn__save" );
+		if ( autosave ) setTimeout ( function ( ) { save . click ( ) }, Math . max ( save_timer, 2 ) * 1000 )
 	}
-	if ( autoeraser ) wiki__text . value = '';
 }
-if ( save && autosave ) save . click ( );
+if ( editor == null ) {
+	const stop = document . querySelector ( ".ui-admin, #dw__register, .idx, #dw__recent, #mediamanager__page, #page__revisions" );
+	if ( autoclose && !stop ) setTimeout ( function ( ) { window . close ( ) }, Math . max ( close_timer, 1 ) * 1000 )
 // в Фаерфоксе требуется about:config -> dom.allow_scripts_to_close_windows -> true
-if ( !stop && autoclose && timer > 2 ) setTimeout ( function ( ) { window . close ( ) }, timer * 1000 )
+}
