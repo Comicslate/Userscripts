b864a6c35e269025714fecd5d8abdbab7b808c66
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2025-06-24 09:12:52 +0900

    Update AutoSaveClicker.user.js

--- Comicslate/AutoSaveClicker.user.js
+++ Comicslate/AutoSaveClicker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate AutoSaveClicker
-// @version			2025.06.23
+// @version			2025.06.24
 // @description		Автоклик сохранения + удалялка
 // @match			http*://*comicslate.org/*
 // @exclude			/^https?://comicslate\.org\/.+publish
@@ -12,7 +12,10 @@
 // @downloadURL		https://github.com/Comicslate/Userscripts/raw/master/Comicslate/AutoSaveClicker.user.js
 // ==/UserScript==
 
-const cf = document . querySelector ( "#cf-wrapper" ),
+// в Фаерфоксе требуется about:config -> dom.allow_scripts_to_close_windows -> true
+
+const date = 'ASCBot 2025.06.24',
+	cf = document . querySelector ( "#cf-wrapper" ),
 	editor = document . querySelector ( ".comic.editpage" ),
 	autoeraser = 0, // для удаления текста - 1 !! ВНИМАНИЕ
 	autosave = 1, // для сохранения - 1, для простого посещения страницы - 0
@@ -29,7 +32,7 @@ if ( editor != null ) {
 		const sum = editor . querySelector ( "#edit__summary" );
 		if ( sum && sum . value != null ) {
 			if ( sum . value != '' ) sum . value += ' / ';
-			sum . value += 'ASCBot 2025.06.23';
+			sum . value += date;
 		}
 		if ( autoeraser ) wiki__text . value = '';
 		const save = editor . querySelector ( "#edbtn__save" );
@@ -39,5 +42,4 @@ if ( editor != null ) {
 if ( editor == null ) {
 	const stop = document . querySelector ( ".ui-admin, #dw__register, .idx, #dw__recent, #mediamanager__page, #page__revisions" );
 	if ( autoclose && !stop ) setTimeout ( function ( ) { window . close ( ) }, Math . max ( close_timer, 1 ) * 1000 )
-// в Фаерфоксе требуется about:config -> dom.allow_scripts_to_close_windows -> true
 }
