9344aba017118b382c5f1a5d90deefb22dfcb9ed
Rainbow Spike <3947636+Rainbow-Spike@users.noreply.github.com>
2025-07-06 22:01:53 +0900

    Update Index EditLinker.user.js

--- Comicslate/Index EditLinker.user.js
+++ Comicslate/Index EditLinker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name			Comicslate Index EditLinker
-// @version			2025.06.22.2
+// @version			2025.07.06
 // @description		Расширение функционала "Индекса"
 // @match			http*://*comicslate.org/*do=index*
 // @match			http*://*comicslate.org/*idx=*
@@ -49,32 +49,30 @@ function InsertButtons ( ) {
 }
 
 function Atr ( num, action ) {
-	document . querySelectorAll ( "#index__tree a:not(.idx_dir)" ) . forEach (
-		function ( e ) {
-			switch ( num ) {
-				case 1:
-					e . href = e . href . split ( '?' ) [ 0 ] + ( action == '' ? '' : '?do=' ) + action;
-					break;
-				case 2:
-					e . href = e . href . replace ( /\/s?..\//, '/' + action + '/' );
-					e . title = e . title . replace ( /^s?..:/, action + ':' );
-					break;
-				case 3:
-					e . href = e . href . split ( '?' ) [ 0 ] + '?do=edit&fromlang=' + action;
-					break;
-				case 4:
-					if ( e . getAttribute ( 'wpn' ) == null ) {
-						e . setAttribute ( 'wpn', e . innerHTML );
-						e . innerHTML = e . href . split ( '/' ) [ e . href . split ( '/' ) . length - 1 ] . split ( '?' ) [ 0 ];
-					} else {
-						e . innerHTML = e . getAttribute ( 'wpn' );
-						e . removeAttribute ( 'wpn' );
-					};
-					break;
-				default: break;
-			}
+	document . querySelectorAll ( "#index__tree a:not(.idx_dir)" ) . forEach ( e => {
+		switch ( num ) {
+			case 1:
+				e . href = e . href . split ( '?' ) [ 0 ] + ( action == '' ? '' : '?do=' ) + action;
+				break;
+			case 2:
+				e . href = e . href . replace ( /\/s?..\//, '/' + action + '/' );
+				e . title = e . title . replace ( /^s?..:/, action + ':' );
+				break;
+			case 3:
+				e . href = e . href . split ( '?' ) [ 0 ] + '?do=edit&fromlang=' + action;
+				break;
+			case 4:
+				if ( e . getAttribute ( 'wpn' ) == null ) {
+					e . setAttribute ( 'wpn', e . innerHTML );
+					e . innerHTML = e . href . split ( '/' ) [ e . href . split ( '/' ) . length - 1 ] . split ( '?' ) [ 0 ];
+				} else {
+					e . innerHTML = e . getAttribute ( 'wpn' );
+					e . removeAttribute ( 'wpn' );
+				};
+				break;
+			default: break;
 		}
-	)
+	} )
 }
 
 InsertButtons ( );
